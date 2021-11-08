52baa28223eb310d70359ad067236657c431615e
Tao Guo <tao.guo@amlogic.com>
2021-11-08 16:06:35 +0800

    Edge use utf16

--- js/has/hasplayer.js
+++ js/has/hasplayer.js
@@ -22957,7 +22957,7 @@ MediaPlayer.dependencies.protection.KeySystem_PlayReady = function() {
 
     var keySystemStr = "com.microsoft.playready",
         keySystemUUID = "9a04f079-9840-4286-ab92-e65be0885f95",
-        messageFormat = "utf8",
+        messageFormat = (navigator.userAgent.match(/Edg/i) || navigator.userAgent.match(/Trident/i)) ? "utf16" : "uft8",
         PRCDMData = '<PlayReadyCDMData type="LicenseAcquisition"><LicenseAcquisition version="1.0" Proactive="false"><CustomData encoding="base64encoded">%CUSTOMDATA%</CustomData></LicenseAcquisition></PlayReadyCDMData>',
         protData,
 
